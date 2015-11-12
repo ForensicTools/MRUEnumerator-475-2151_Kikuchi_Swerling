8294309bf7c1bc017f8bcfae34deb04664469753
stk4702 <stk4702@rit.edu>
2015-11-12 01:56:04 -0500

    v1.2; adds support for Adobe Reader MRUs

--- mrue.pl
+++ mrue.pl
@@ -78,18 +78,15 @@ foreach ( @array ){
 			if ( $_ =~ m/^  Item./ ){
 				push @word, $_;
 			}
-		}
-		elsif ( $section == 2 ){
+		} elsif ( $section == 2 ){
 			if ( $_ =~ m/^  Item./ ){
 				push @excel, $_;
 			}
-		}
-		elsif ( $section == 3 ){
+		} elsif ( $section == 3 ){
 			if ( $_ =~ m/^  Item./ ){
 				push @access, $_;
 			}
-		}
-		elsif ( $section == 4 ){
+		} elsif ( $section == 4 ){
 			if ( $_ =~ m/^  Item./ ){
 				push @powerpoint, $_;
 			}
@@ -106,15 +103,145 @@ my $powerpointsize = scalar( @powerpoint );
 #calculate process & print flag
 my $ppflag = $wordsize . $excelsize . $accesssize . $powerpointsize;
 
+#did office run flag
+my $office2010rf = -1;
+
 #if larger than 0000, call office2010pp sub
 if ( $ppflag > 0 )
 {
+	$office2010rf = 1;
 	&office2010pp;
-}
-else
-{
+
+#else print user message
+} else {
+	$office2010rf = 0;
 	print "No Office 2010 data found.\n";
 }
+
+##########
+#adoberdr#
+##########
+
+#call rip.pl, use the "officedocs" plugin, and put the output in a temporary file
+system( qq{ perl rip.pl -r ./EVIDENCE/NTUSER.DAT -p adoberdr > "./TMP/output.tmp" });
+
+#open the output file or die
+open( $fh, "<", "./TMP/output.tmp" )
+	or die "Fatal Error - Cannot open output.tmp.\n";
+
+#read the contents of the file into an array
+while( <$fh> ){
+	chomp;
+	push @array, $_;
+}
+
+#close the file handle
+close $fh;
+
+#array to hold output
+my @adoberdr;
+
+#sort data
+foreach ( @array ){
+	#if the line is a key, push it into the main data array
+	if ( $_ =~ m/^c[0-9]*[0-9]*[0-9],./ ){
+		push @adoberdr, $_;
+	}
+}
+
+#pull out just the path
+my @temp;
+
+#split each element into its three parts
+#replace the current element with just the full path
+#remove the "/" at the beginning of each element
+#replace all "\" with "/"
+#grab the filename from the full path and throw away everything else
+#number all elements
+#increment counter
+#reset counter after each loop
+my $counter = 1;
+foreach( @adoberdr ){
+	my @temp = split( ',', $_ );
+	$_ = $temp[ 1 ];
+	$_ = substr( $_, 1, -1 );
+	$_ =~ s/\\/\//g;
+	$_ = fileparse( $_ );
+	$_ = "$counter $_";
+	$counter++;
+}
+
+#if office2010 did not produce results create a new file
+if( $office2010rf == 0 ){
+
+	#open file handle in order to make output json
+	open( $fh, ">", "./RESULTS/data.json" )
+		or die "Fatal Error - Cannot create output json.\n";
+
+	#print header data to json
+	print $fh "{\n";
+	print $fh " \"name\": \"Main\",\n";
+	print $fh " \"children\": [\n";
+	print $fh "  {\n";
+	print $fh "   \"name\": \"Adobe Reader\",\n";
+	print $fh "   \"children\": [\n";
+	print $fh "    {\n";
+
+	
+
+#else open and append to the existing file
+} else {
+	#open file handle in order to make output json
+	open( $fh, ">>", "./RESULTS/data.json" )
+		or die "Fatal Error - Cannot open output json.\n";
+
+	print $fh "  {\n";
+	print $fh "   \"name\": \"Adobe Reader\",\n";
+	print $fh "   \"children\": [\n";
+}
+
+#get size of word array
+my $arraysize = scalar( @adoberdr );
+
+#just print nothing
+if( $arraysize == 0 ){
+	print $fh "     {}\n";
+	
+#print the only element
+} elsif ( $arraysize == 1 ){
+	#print the last element
+	my $lastelement = pop @adoberdr;
+	print $fh "    {\"name\": \"$lastelement\", \"size\": 5000}\n";
+	
+#print element 0 & 1
+} elsif ( $arraysize == 2 ){
+	$arraysize = 1;
+
+	#print all but the last element
+	foreach my $x ( 0..$arraysize ){
+		print $fh "    {\"name\": \"$adoberdr[ $x ]\", \"size\": 5000},\n";
+	}
+	
+#all other array sizes (2+)
+} else {
+	#subtract one to account for index 0, another to omit the last element
+	$arraysize = $arraysize - 2;
+
+	#print all but the last element
+	foreach my $x ( 0..$arraysize ){
+		print $fh "    {\"name\": \"$adoberdr[ $x ]\", \"size\": 5000},\n";
+	}
+
+	#print the last element
+	my $lastelement = pop @adoberdr;
+	print $fh "    {\"name\": \"$lastelement\", \"size\": 5000}\n";
+}
+
+#print closing braces
+print $fh "   ]\n";
+print $fh "  }\n";
+print $fh " ]\n";
+print $fh "}\n";
 
 ##################
 #officedocs2010pp#
@@ -189,25 +316,24 @@ sub office2010pp
 	#just print nothing
 	if( $arraysize == 0 ){
 		print $fh "       {}\n";
-	}
 
-	#print the only element
-	elsif ( $arraysize == 1 ){
+	#print the only element	
+	} elsif ( $arraysize == 1 ){
 		#print the last element
 		my $lastelement = pop @word;
 		print $fh "      {\"name\": \"$lastelement\", \"size\": 5000}\n";
-	}
+	
 	#print element 0 & 1
-	elsif ( $arraysize == 2 ){
+	} elsif ( $arraysize == 2 ){
 		$arraysize = 1;
 
 		#print all but the last element
 		foreach my $x ( 0..$arraysize ){
 			print $fh "      {\"name\": \"$word[ $x ]\", \"size\": 5000},\n";
 		}
-	}
+
 	#all other array sizes (2+)
-	else {
+	} else {
 		#subtract one to account for index 0, another to omit the last element
 		$arraysize = $arraysize - 2;
 
@@ -236,25 +362,24 @@ sub office2010pp
 	#just print nothing
 	if( $arraysize == 0 ){
 		print $fh "       {}\n";
-	}
-
+	
 	#print the only element
-	elsif ( $arraysize == 1 ){
+	} elsif ( $arraysize == 1 ){
 		#print the last element
 		my $lastelement = pop @excel;
 		print $fh "      {\"name\": \"$lastelement\", \"size\": 5000}\n";
-	}
-	#print element 0 & 1
-	elsif ( $arraysize == 2 ){
+
+	#print element 0 & 1	
+	} elsif ( $arraysize == 2 ){
 		$arraysize = 1;
 
 		#print all but the last element
 		foreach my $x ( 0..$arraysize ){
 			print $fh "      {\"name\": \"$powerpoint[ $x ]\", \"size\": 5000},\n";
 		}
-	}
+
 	#all other array sizes (2+)
-	else {
+	} else {
 		#subtract one to account for index 0, another to omit the last element
 		$arraysize = $arraysize - 2;
 
@@ -283,25 +408,24 @@ sub office2010pp
 	#just print nothing
 	if( $arraysize == 0 ){
 		print $fh "       {}\n";
-	}
-
+	
 	#print the only element
-	elsif ( $arraysize == 1 ){
+	} elsif ( $arraysize == 1 ){
 		#print the last element
 		my $lastelement = pop @access;
 		print $fh "      {\"name\": \"$lastelement\", \"size\": 5000}\n";
-	}
-	#print element 0 & 1
-	elsif ( $arraysize == 2 ){
+	
+	#print element 0 & 1	
+	} elsif ( $arraysize == 2 ){
 		$arraysize = 1;
 
 		#print all but the last element
 		foreach my $x ( 0..$arraysize ){
 			print $fh "      {\"name\": \"$access[ $x ]\", \"size\": 5000},\n";
 		}
-	}
+	
 	#all other array sizes (2+)
-	else {
+	} else {
 		#subtract one to account for index 0, another to omit the last element
 		$arraysize = $arraysize - 2;
 
@@ -330,25 +454,24 @@ sub office2010pp
 	#just print nothing
 	if( $arraysize == 0 ){
 		print $fh "       {}\n";
-	}
-
+	
 	#print the only element
-	elsif ( $arraysize == 1 ){
+	} elsif ( $arraysize == 1 ){
 		#print the last element
 		my $lastelement = pop @powerpoint;
 		print $fh "      {\"name\": \"$lastelement\", \"size\": 5000}\n";
-	}
+	
 	#print element 0 & 1
-	elsif ( $arraysize == 2 ){
+	} elsif ( $arraysize == 2 ){
 		$arraysize = 1;
 
 		#print all but the last element
 		foreach my $x ( 0..$arraysize ){
 			print $fh "      {\"name\": \"$powerpoint[ $x ]\", \"size\": 5000},\n";
 		}
-	}
+	
 	#all other array sizes (2+)
-	else {
+	} else {
 		#subtract one to account for index 0, another to omit the last element
 		$arraysize = $arraysize - 2;
 
@@ -366,9 +489,7 @@ sub office2010pp
 	print $fh "     ]\n";
 	print $fh "    }\n";
 	print $fh "   ]\n";
-	print $fh "  }\n";
-	print $fh " ]\n";
-	print $fh "}\n";
+	print $fh "  },\n";
 }
 
 
